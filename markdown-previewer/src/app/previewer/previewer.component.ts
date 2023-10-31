0d052be074afa4346be23a0a77ce5504fcb30d0d
Steve <stevewu24@gmail.com>
2023-10-31 02:16:17 -0700

    added boiler plate

---
+++ markdown-previewer/src/app/previewer/previewer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-previewer',
+  templateUrl: './previewer.component.html',
+  styleUrls: ['./previewer.component.css']
+})
+export class PreviewerComponent {
+
+}
